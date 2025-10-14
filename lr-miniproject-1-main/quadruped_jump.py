7a0d8e86def84e2ef004d58d8d6c07150d68c1c1
Johanne <johanne.pinel@epfl.ch>
2025-10-14 15:44:34 +0200

    estelle_branch

--- lr-miniproject-1-main/quadruped_jump.py
+++ lr-miniproject-1-main/quadruped_jump.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from env.simulation import QuadSimulator, SimulationOptions
 
 from profiles import FootForceProfile
