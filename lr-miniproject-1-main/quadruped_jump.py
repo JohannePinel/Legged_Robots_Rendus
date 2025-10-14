5b77836ad8ae73bf1092b0164e95da8969122ead
Johanne <johanne.pinel@epfl.ch>
2025-10-14 14:08:28 +0200

    recuperation main sur joh

--- lr-miniproject-1-main/quadruped_jump.py
+++ lr-miniproject-1-main/quadruped_jump.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from env.simulation import QuadSimulator, SimulationOptions
 
 from profiles import FootForceProfile
@@ -21,11 +21,11 @@ def quadruped_jump():
 
     # Determine number of jumps to simulate
     n_jumps = 10  # Feel free to change this number
-    jump_duration = 5.0  # TODO: determine how long a jump takes
+    jump_duration = 1.0  # TODO: determine how long a jump takes
     n_steps = int(n_jumps * jump_duration / sim_options.timestep)
 
     # TODO: set parameters for the foot force profile here
-    force_profile = FootForceProfile(f0=0, f1=0, Fx=0, Fy=0, Fz=0)
+    force_profile = FootForceProfile(f0=0.2, f1=0.2, Fx=0, Fy=0, Fz=100)
 
     for _ in range(n_steps):
         # If the simulator is closed, stop the loop
