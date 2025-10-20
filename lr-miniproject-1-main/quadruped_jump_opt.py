4c8a86b69355a13d2d921c779ec7de114e0aa6fe
Matt <matt.anner@epfl.ch>
2025-10-20 16:03:34 +0200

    opti pour clockwise furthest ok

--- lr-miniproject-1-main/quadruped_jump_opt.py
+++ lr-miniproject-1-main/quadruped_jump_opt.py
@@ -67,8 +67,8 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
     f0 = trial.suggest_float(name="f0", low = 0.5, high = 5)
     f1 = trial.suggest_float(name="f1", low = 0.5, high = 5)
     Fx = trial.suggest_float(name="Fx", low = -5, high = 5)
-    Fy = trial.suggest_float(name="Fy", low = -150, high = -50)
-    Fz = trial.suggest_float(name="Fz", low = 75, high = 200)
+    Fy = trial.suggest_float(name="Fy", low = 25, high = 100)
+    Fz = trial.suggest_float(name="Fz", low = 50, high = 200)
      
     # Reset the simulation
     simulator.reset()
@@ -84,7 +84,7 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
     TWIST_CLOCK_JUMP = 3 #works but not ideal
     TWIST_COUNTER_CLOCK_JUMP = 4 #works but not ideal
 
-    jump_type =  LATERAL_JUMP_RIGHT
+    jump_type =  TWIST_CLOCK_JUMP
 
     
     # TODO: set parameters for the foot force profile here
@@ -98,7 +98,8 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
     total_time = n_steps * sim_options.timestep #to measure fastest controller
     #max_height = -99 # to initialize tracking
     start_pos = simulator.get_base_position()[0] #to measure fastest controller
-
+    yaw_previous = 0
+    yaw_offset = 0
     for _ in range(n_steps):
         # Step the oscillator
         force_profile.step(sim_options.timestep)
@@ -127,7 +128,22 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
                 tau -= nominal_position(simulator)
                 des_foot_position = np.array([[0,-0.4, 0.1],[0,0.1, -0.3],[0,-0.4, 0.1],[0,0.1, -0.2]])
                 tau += nominal_position(simulator, des_foot_position)
-
+            elif jump_type == 3 :
+                tau -= nominal_position(simulator)
+                des_foot_position = np.array([[0,-0.2, -0.2],[0,0, -0.2],[0,0, -0.2],[0,0.2, -0.2]]) #sifht d'environ -0.1 pour les jambes avant et + 0.1 pou les jambes arriere
+                tau += nominal_position(simulator, des_foot_position)
+            elif jump_type == 4 :
+                tau -= nominal_position(simulator)
+                des_foot_position = np.array([[0,0, -0.2],[0,0.2, -0.2],[0,-0.2, -0.2],[0,0, -0.2]])
+                tau += nominal_position(simulator, des_foot_position)
+        #update the yaw with each step ans makes it possible to count turns
+        _, _, yaw = simulator.get_base_orientation_roll_pitch_yaw()
+        if (yaw - yaw_previous) >np.pi:
+            yaw_offset -= 2*np.pi
+        elif(yaw - yaw_previous) < -np.pi:
+            yaw += 2*np.pi
+        yaw_previous = yaw
+        yaw_final = yaw + yaw_offset
         # Set the motor commands and step the simulation
         simulator.set_motor_targets(tau)
         simulator.step()
@@ -144,8 +160,8 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
     #TO MEASURE FURTHEST
     
     position = simulator.get_base_position()
-    roll, pitch, yaw = simulator.get_base_orientation_roll_pitch_yaw()
-
+    
+    
     if jump_type == FORWARD_JUMP:
         # Further along X
         objective_value = position[0]
@@ -156,15 +172,15 @@ def evaluate_jumping(trial: Trial, simulator: QuadSimulator) -> float:
 
     elif jump_type == LATERAL_JUMP_RIGHT:
         # Negative Y direction (absolute)
-        objective_value = -position[1]/n_jumps  # or abs(position[1])
+        objective_value = -position[1]  # or abs(position[1])
 
     elif jump_type == TWIST_CLOCK_JUMP:
         # Max clockwise twist → NEGATIVE yaw (assuming right-hand rule)
-        objective_value = -yaw
+        objective_value = -yaw_final #proble yax est en -pi et +pi et prends en compte que la position finale
 
     elif jump_type == TWIST_COUNTER_CLOCK_JUMP:
         # Max counterclockwise twist → POSITIVE yaw
-        objective_value = yaw
+        objective_value = yaw_final
 
     return objective_value
     """
